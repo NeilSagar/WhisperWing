0883cb75b851c336455a297ecb61ffc81bb67cc9
Neil Sagar Sahu <neilsagar55555@gmail.com>
2024-02-07 23:52:20 +0530

    removed unused files

--- frontend/src/connection/socketConnection.js
+++
@@ -1,8 +0,0 @@
-import {io} from "socket.io-client";
-
-const URL = "http://localhost:5000";
-
-export const connectSocket = (token) => {
-    const socket = io.connect(URL, { auth: { token: token } });
-    return socket;
-};
